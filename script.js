24e6bb71efac5af574396abd3fa3c36f38ee6542
Adeyemi Opeyemi <107158438+Adeopemi@users.noreply.github.com>
2023-06-28 04:02:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Process the form submission using AJAX or any other method you prefer
+    // Example: Send the form data to the server and display a success message
+    alert('Thank you for contacting me! I will get back to you soon.');
+  });
+  
